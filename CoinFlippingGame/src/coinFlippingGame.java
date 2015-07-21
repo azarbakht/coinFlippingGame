22f23c57a85e3fb2b0124e901cf45a601cbf82af
Emerson Amirhosein Azarbakht <azarbaam@eecs.oregonstate.edu>
2015-07-21 12:19:07 -0700

    added isStopCase method

--- CoinFlippingGame/src/coinFlippingGame.java
+++ CoinFlippingGame/src/coinFlippingGame.java
@@ -9,66 +9,56 @@ public class coinFlippingGame {
 
 	public static void main(String[] args) {
 
+		countValidCoinGame(8, 0);
 
-		//		int upperBound = (int) Math.pow(2, 8);
-		int count0, count1, validCaseCount =0, totalValid = 0;
-		//
-		//		for(int i = 0; i < upperBound; i++){
-		//
-		//			System.out.println(i);
-		//			System.out.printf("Binary representation: %20s \n", Integer.toBinaryString(i));
-		//			System.out.printf("numberOfHeads = %20d \n" , Integer.bitCount(i));
-		//			System.out.printf("numberf of coin flips = %12d \n" , Integer.toBinaryString(i).length());
-		//
-		//			int numberOfOnesHeads = Integer.bitCount(i);
-		//			int numberOfCoinFlips = Integer.toBinaryString(i).length();
-		//			int numberOfZerosTails = numberOfCoinFlips - numberOfOnesHeads;
-		//
-		//			if (numberOfOnesHeads == numberOfZerosTails){
-		//					validCaseCount++;
-		//			}
-		//
-		//			System.out.printf("Number of Valid cases = %13d \n", validCaseCount);
-		//
-		//		}
-
-//		stopCasesBefore8th(2, 0);
-//		stopCasesBefore8th(4, 2);
-//		stopCasesBefore8th(6, 16);
-//		stopCasesBefore8th(8, 64);
-		stopCasesBefore8th(8, 0);
 	}
 
-	public static void stopCasesBefore8th(int totalNumberOfFlips, int lowerBound){
+	public static void countValidCoinGame(int totalNumberOfFlips, int lowerBound){
 
 		int upperBound = (int) Math.pow(2, totalNumberOfFlips);
 		int validCaseCount = 0;
 		int stopCaseCount = 0;
 
+		System.out.printf("LowerBound: %d \n UpperBound: %d \n", lowerBound, upperBound);
+
 		for(int i= lowerBound; i < upperBound; i++){
-			System.out.printf("Binary representation of i: *** %20s *** \n", Integer.toBinaryString(i));
-
-			int numberOfHeads = Integer.bitCount(i);
-			int numberOfTails = Integer.toBinaryString(i).length() - numberOfHeads;
-			System.out.printf("Number of Heads: %31d \n", numberOfHeads);
-			System.out.printf("Length of binary representation: %20d \n", Integer.toBinaryString(i).length());
-			System.out.printf("Number of Tails: %31d \n", numberOfTails);
-			System.out.println("**************************************************************");
+			String binaryStringI = Integer.toBinaryString(i);
+			System.out.printf("Binary representation of i: *** %20s *** \n", binaryStringI);
+			int numberOfHeads =0;
+			int numberOfTails = 0;
 
 
-			if (numberOfHeads == numberOfTails){
-				System.out.println("STOP CASE HERE $#$#$$#$#$$#$#$##$#$#$#$#$#$#$#$#$#$");
-				if (numberOfHeads != 4){
-					stopCaseCount++;
-				} else if (numberOfHeads == 4){
-					validCaseCount++;	
-				}
 
+		} // for 0-256
+
+		System.out.printf("Total Stop Case Count: %d", stopCaseCount);
+
+	} // method countValidCoinGame
+
+	public static int isStopCase(String binaryString) {
+		int numberOfHeads = 0;
+		int numberOfTails = 0;
+		for (int j = 0; j < binaryString.length(); j++){
+			String charAtJ = Character.toString(binaryString.charAt(j));
+			System.out.println(charAtJ);
+
+			if (charAtJ.equals("1")){
+				numberOfHeads ++;
+			} else if(charAtJ.equals("0")){
+				numberOfTails ++;
 			}
-
 		}
-		System.out.printf("number of Valid Cases: %10d \n number of Stop Cases != 4: %10d", validCaseCount, stopCaseCount);
 
+		System.out.println(numberOfHeads);
+		System.out.println(numberOfTails);
+
+		if (numberOfHeads == numberOfTails){
+			System.out.println("STOP CASE HERE $#$#$$#$#$$#$#$##$#$#$#$#$#$#$#$#$#$");
+			return 0;
+		} else if (numberOfHeads != numberOfTails){
+			return 1;
+		}
+		return 1;
 	}
 
-}
+} //class
